a1653be3fa080767254a9a4376ad6bc0772776bb
alltonp <paul.allton@theorem.technology>
2015-05-13 17:56:23 +0100

    scala 2.11.6 and oracle 8

--- src/main/scala/im/mange/little/amount/AmountFormats.scala
+++ src/main/scala/im/mange/little/amount/AmountFormats.scala
@@ -3,7 +3,6 @@ package im.mange.little.amount
 import java.text.DecimalFormat
 
 object AmountFormats {
-  // TODO: should make a nice little builder and jetboot
   val `0dp` = new DecimalFormat("#,##0")
   val `1dp` = new DecimalFormat("#,##0.0")
   val `2dp` = new DecimalFormat("#,##0.00")
