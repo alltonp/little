aaa5f3446bd4d1e02223c3d5579203c8be8328a5
alltonp <paul.allton@theorem.technology>
2015-05-26 12:32:27 +0100

    toding

--- src/main/scala/im/mange/little/clock/FrozenClock.scala
+++ src/main/scala/im/mange/little/clock/FrozenClock.scala
@@ -2,6 +2,7 @@ package im.mange.little.clock
 
 import org.joda.time.{DateTime, DateTimeZone}
 
+//TODO: we should probably free to a more sensible date or just make it mandatory
 case class FrozenClock(value: DateTime = new DateTime(2015, 1, 1, 9, 0, 0, DateTimeZone.UTC)) extends Clock {
   def date = value.toLocalDate
   def dateTime = value
