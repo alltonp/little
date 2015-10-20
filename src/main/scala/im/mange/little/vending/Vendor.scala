e5ca1da274415549d2652d82ec95dc089739fd21
Franck Rasolo <franck.rasolo@gmail.com>
2015-10-20 17:33:19 +0100

    Vend only once per assigned lambda

--- src/main/scala/im/mange/little/vending/Vendor.scala
+++ src/main/scala/im/mange/little/vending/Vendor.scala
@@ -4,9 +4,10 @@ trait Vendor {
 
   private[vending] class Lazy[T] {
     private var f: () ⇒ T = _
+    private var vended: Option[T] = _
     def apply(): T = value
-    def value: T = f()
-    def value_=(f: ⇒ T) = this.f = () ⇒ f
+    def value: T = { if (vended.isEmpty) vended = Some(f()); vended.get }
+    def value_=(f: ⇒ T) = { this.f = () ⇒ f; vended = None }
   }
 
   protected def set[T](initializer: ⇒ T) = { val v = new Lazy[T](); v.value = initializer; v }
