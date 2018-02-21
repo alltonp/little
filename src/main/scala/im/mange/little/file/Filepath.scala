aa84e30acf177ebbb045210042cbf61eefaf9d80
pall <paulallton@gmail.com>
2018-02-21 12:12:58 +0000

    add list and add return types

--- src/main/scala/im/mange/little/file/Filepath.scala
+++ src/main/scala/im/mange/little/file/Filepath.scala
@@ -4,17 +4,25 @@ import java.nio.charset.StandardCharsets._
 import java.nio.file.Files._
 import java.nio.file.StandardCopyOption._
 import java.nio.file.StandardOpenOption._
-import java.nio.file.{Paths, Files, Path}
+import java.nio.file.{DirectoryStream, Files, Path, Paths}
 
 import scala.reflect.io.File
 
 object Filepath {
-  def save(content: String, path: Path)   = write(path, content.getBytes(UTF_8), CREATE, WRITE, TRUNCATE_EXISTING)
-  def append(content: String, path: Path) = write(path, content.getBytes(UTF_8), CREATE, WRITE, APPEND)
+  def load(path: Path): String = File(path.toFile).slurp()
+
+  def save(content: String, path: Path): Path = write(path, content.getBytes(UTF_8), CREATE, WRITE, TRUNCATE_EXISTING)
+  def append(content: String, path: Path): Path = write(path, content.getBytes(UTF_8), CREATE, WRITE, APPEND)
 
   def move(source: Path, target: Path): Path = Files.move(source, target, ATOMIC_MOVE)
   def move(source: File, target: File): Path = move(Paths.get(source.path), Paths.get(target.path))
 
-  def create(path: Path) = createFile(path)
-  def load(path: Path)   = File(path.toFile).slurp()
+//  def create(path: Path): Path = createFile(path)
+
+  def list(dir: Path, glob: String): List[String] = {
+    val stream: DirectoryStream[Path] = Files.newDirectoryStream(dir, glob)
+    var result: List[String] = Nil
+    stream.forEach(f => result = f.getFileName.toFile.getName :: result)
+    result
+  }
 }
