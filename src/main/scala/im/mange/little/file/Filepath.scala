e0fa7ae1a791018820f46ca601118421ecc13881
pall <paulallton@gmail.com>
2018-02-21 12:55:02 +0000

    add copy and re-jig a bit

--- src/main/scala/im/mange/little/file/Filepath.scala
+++ src/main/scala/im/mange/little/file/Filepath.scala
@@ -1,6 +1,5 @@
 package im.mange.little.file
 
-import java.nio.file.Files._
 import java.nio.file.StandardCopyOption._
 import java.nio.file.StandardOpenOption._
 import java.nio.file.{DirectoryStream, Files, Path, Paths}
@@ -9,19 +8,28 @@ import scala.io.Codec
 import scala.reflect.io.File
 
 object Filepath {
-  def load(path: Path, codec: Codec = Codec.UTF8): String = File(path.toFile).slurp(codec)
+  def load(path: Path, codec: Codec = Codec.UTF8): String =
+    File(path.toFile).slurp(codec)
+//  or Files.readAllLines(path).asScala.mkString("\n")
 
   def save(content: String, path: Path, codec: Codec = Codec.UTF8): Path =
-    write(path, content.getBytes(codec.charSet), CREATE, WRITE, TRUNCATE_EXISTING)
+    Files.write(path, content.getBytes(codec.charSet), CREATE, WRITE, TRUNCATE_EXISTING)
 
   def append(content: String, path: Path, codec: Codec = Codec.UTF8): Path =
-    write(path, content.getBytes(codec.charSet), CREATE, WRITE, APPEND)
+    Files.write(path, content.getBytes(codec.charSet), CREATE, WRITE, APPEND)
 
-  //TODO: kill this soon, if not needed
+  //TODO: kill these soon, if not needed
   //def touch(path: Path): Path = createFile(path)
+//  def exists(path: Path) = Files.exists(path)
 
-  def move(source: Path, target: Path): Path = Files.move(source, target, ATOMIC_MOVE)
-  def move(source: File, target: File): Path = move(Paths.get(source.path), Paths.get(target.path))
+  def move(source: Path, target: Path): Path =
+    Files.move(source, target, ATOMIC_MOVE, REPLACE_EXISTING)
+
+  def copy(source: Path, target: Path): Path =
+    Files.copy(source, target, ATOMIC_MOVE, REPLACE_EXISTING)
+
+  //TODO: not sure about this version, prefer string for overloads
+//  def move(source: File, target: File): Path = move(Paths.get(source.path), Paths.get(target.path))
 
   def createDir(dir: Path): Path = Files.createDirectories(dir)
 
@@ -31,6 +39,8 @@ object Filepath {
     stream.forEach(f => result = f.getFileName.toFile.getName :: result)
     result
   }
+
+  def toPath(value: String) = Paths.get(value)
 
   //TODO: add deleteDir(dir: Path)
 
