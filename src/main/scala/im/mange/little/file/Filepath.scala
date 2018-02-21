83c1f2de8498b2f33e15ff7c920831356e5f1484
pall <paulallton@gmail.com>
2018-02-21 12:32:40 +0000

    todoing

--- src/main/scala/im/mange/little/file/Filepath.scala
+++ src/main/scala/im/mange/little/file/Filepath.scala
@@ -1,7 +1,5 @@
 package im.mange.little.file
 
-import java.nio.charset.Charset
-import java.nio.charset.StandardCharsets._
 import java.nio.file.Files._
 import java.nio.file.StandardCopyOption._
 import java.nio.file.StandardOpenOption._
@@ -13,24 +11,28 @@ import scala.reflect.io.File
 object Filepath {
   def load(path: Path, codec: Codec = Codec.UTF8): String = File(path.toFile).slurp(codec)
 
-  def save(content: String, path: Path, charset: Charset = UTF_8): Path =
-    write(path, content.getBytes(charset), CREATE, WRITE, TRUNCATE_EXISTING)
+  def save(content: String, path: Path, codec: Codec = Codec.UTF8): Path =
+    write(path, content.getBytes(codec.charSet), CREATE, WRITE, TRUNCATE_EXISTING)
 
-  def append(content: String, path: Path, charset: Charset = UTF_8): Path =
-    write(path, content.getBytes(charset), CREATE, WRITE, APPEND)
+  def append(content: String, path: Path, codec: Codec = Codec.UTF8): Path =
+    write(path, content.getBytes(codec.charSet), CREATE, WRITE, APPEND)
+
+  //TODO: kill this soon, if not needed
+  //def touch(path: Path): Path = createFile(path)
 
   def move(source: Path, target: Path): Path = Files.move(source, target, ATOMIC_MOVE)
   def move(source: File, target: File): Path = move(Paths.get(source.path), Paths.get(target.path))
 
-  //TODO: not sure we need this, so excluding for a bit
-  //def create(path: Path): Path = createFile(path)
-
   def createDir(dir: Path): Path = Files.createDirectories(dir)
 
-  def list(dir: Path, glob: String): List[String] = {
+  def listDir(dir: Path, glob: String): List[String] = {
     val stream: DirectoryStream[Path] = Files.newDirectoryStream(dir, glob)
     var result: List[String] = Nil
     stream.forEach(f => result = f.getFileName.toFile.getName :: result)
     result
   }
+
+  //TODO: add deleteDir(dir: Path)
+
+  //TODO: maybe the dir ones should be on a different object ... again
 }
