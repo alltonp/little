656647a8db25dbc999ebb28a4e261b82721ee21d
pall <paulallton@gmail.com>
2018-02-23 13:56:27 +0000

    tidying

--- src/main/scala/im/mange/little/file/Filepath.scala
+++ src/main/scala/im/mange/little/file/Filepath.scala
@@ -24,17 +24,11 @@ object Filepath {
   def append(content: String, path: Path, codec: Codec = Codec.UTF8): Path =
     write(content, path, codec, CREATE, WRITE, APPEND)
 
-  //TODO: kill these soon, if not needed
-  //def touch(path: Path): Path = createFile(path)
-
   def move(source: Path, target: Path): Path =
     Files.move(source, target, ATOMIC_MOVE, REPLACE_EXISTING)
 
   def copy(source: Path, target: Path): Path =
     Files.copy(source, target, ATOMIC_MOVE, REPLACE_EXISTING)
-
-  //TODO: not sure about this version, prefer string for overloads
-//  def move(source: File, target: File): Path = move(Paths.get(source.path), Paths.get(target.path))
 
   def createDir(dir: Path): Path = Files.createDirectories(dir)
 
@@ -56,7 +50,10 @@ object Filepath {
 
   private def exists(path: Path) = Files.exists(path)
 
-  //TODO: consider string values instead of/as well as Path
+  //TIP: keep api to use Path, means the client will be less stringy
   //TODO: maybe the dir ones should be on a different object ... again
   //  def toPath(value: String) = Paths.get(value)
+
+  //TODO: kill this soon, if def not needed - it's basically save("") anyway
+  //def touch(path: Path): Path = createFile(path)
 }
